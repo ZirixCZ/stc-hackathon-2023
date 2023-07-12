73626c22418a774c8b103c797474c1e869bf0d2e
Michal Vaniš <zirixcz@gmail.com>
2023-07-12 23:59:01 +0200

    phrase return func WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { useRef } from "react";
 import { Button, Text, Input, Box, Flex } from "@chakra-ui/react";
 import dateToDefaultValue from "@/utils/dateDefaultValue";
 
 export default function Home() {
+  const dateRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = () => {
+    if (!dateRef.current) return;
+
+    const date = dateRef.current.value;
+    console.log(date);
+  };
+
   return (
-    <Flex alignItems="center" justifyContent="center" w="100%" h="100%">
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      w="100%"
+      h="100%"
+      direction="column"
+      gap="1rem"
+    >
       <Box w="16rem">
         <Text>Date of birth</Text>
         <Input
@@ -15,6 +32,7 @@ export default function Home() {
           defaultValue={dateToDefaultValue(new Date(Date.now()))}
         />
       </Box>
+      <Button onClick={handleSubmit}>Submit</Button>
     </Flex>
   );
 }
